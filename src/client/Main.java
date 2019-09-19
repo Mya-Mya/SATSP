f7da951ee25c55b21e0d33f86693eaaa1cbc6c22
Mya-Mya <interleukin413@gmail.com>
2019-09-19 22:03:25 +0900

    none

--- src/client/Main.java
+++ src/client/Main.java
@@ -4,6 +4,7 @@ import ui.MainFrame;
 
 public class Main {
     public static void main(String args[]) {
+        //new MyTest();
         System.out.println("HelloWorld");
         new MainFrame();
     }
